8a596a2c035236096326ef554f9df0fcd25ce604
Zixuan Wang <33987311+zixuan75@users.noreply.github.com>
2022-06-17 15:32:38 -0400

    forgot to commit this lol

--- zEngine/util/vector/Vector.java
+++ zEngine/util/vector/Vector.java
@@ -1,10 +1,5 @@
 package zEngine.util.vector;
 
-/*
- * ALl of this code was made months ago and I don't want to rewrite it or
- * provide any documentation because I'm too lazy to do so.
- */
-
 public interface Vector {
     public int size();
 	public float get(int i);
